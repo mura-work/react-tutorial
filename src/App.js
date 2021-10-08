c97d8c913c3f6ea4ee3e0228c44f6d829eb0652d
murata <murata.small.baseball@gmail.com>
2021-10-08 22:32:20 +0900

    buttonコンポーネント配置

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
+import Button from './button'
 
 function App() {
-  //creating IP state
   return (
     <div className="App">
       <h3>Material Ui</h3>
+      <Button >aaa</Button>
     </div>
   );
 }
+
 
 
 export default App;
