68abf2762d795063a16a0f3b1cb085dabae024c7
murata <murata.small.baseball@gmail.com>
2021-10-09 09:24:17 +0900

    ルーティングの設置

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import { BrowserRouter,  Route, Switch, Link } from 'react-router-dom';
 import './App.css';
-import Button from './button'
+import Game from './game'
+import Top from './top'
 
 function App() {
   return (
-    <div className="App">
-      <h3>Material Ui</h3>
-      <Button >aaa</Button>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={Top} />
+        <Route path='/game' component={Game} />
+      </Switch>
+      <Link to='/'>Back To Top</Link>
+    </BrowserRouter>
   );
 }
 
---
+++ src/top.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Top extends React.Component {
+  render() {
+    return (
+      <h1>トップページ</h1>
+    )
+  }
+}
+
+export default Top
